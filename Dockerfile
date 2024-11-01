195659028e497d5d071e79d092390a407b6e0564
Fabien MARTY <fabien.marty@botify.com>
2024-11-01 09:57:29 +0100

    use v0.5.3 version

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,10 @@ FROM alpine:latest
 RUN apk add git curl
 COPY download.sh /app/bin/
 
-# Download version v0.4.0
+# Download version v0.5.3
 RUN mkdir -p /app/bin && \
     cd /app/bin && \
-    ./download.sh "https://api.github.com/repos/fabien-marty/github-next-semantic-version/releases/182114439" && \
+    ./download.sh "https://api.github.com/repos/fabien-marty/github-next-semantic-version/releases/183066274" github-next-semantic-version && \
     chmod +x github-next-semantic-version 
 
 COPY entrypoint.sh /
--- download.sh
+++ download.sh
@@ -1,18 +1,31 @@
 #!/bin/sh
 
 URL=$1
+NAME=$2
 if test "${URL}" = ""; then
     echo "ERROR: missing URL"
     exit 1
 fi
+if test "${NAME}" = ""; then
+    echo "ERROR: missing NAME"
+    exit 1
+fi
 echo "URL=${URL}"
+echo "NAME=${NAME}"
 
-ARCH=${2:-linux-amd64}
+ARCH=${3:-linux-amd64}
 echo "ARCH=${ARCH}"
+
+curl -s "${URL}" \
+| grep "browser_download_url" \
+| grep "${ARCH}" \
+| grep "/${NAME}/" \
+| grep -v "md5" 
 
 LOCATION=$(curl -s "${URL}" \
 | grep "browser_download_url" \
 | grep "${ARCH}" \
+| grep "/${NAME}-v" \
 | grep -v "md5" \
 | awk '{ print $2 }' \
 | sed 's/,$//'       \
